e1b1f7ee1eeeb6118bd02f7122f030d056b49445
ekinncem <ekinn.ciem16@gmail.com>
2024-12-23 19:29:10 +0300

    new

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Note App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueGrey,
+        colorScheme: ColorScheme.dark(primary: Colors.cyan),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white70),
+        ),
+      ),
+      home: const NoteHome(),
+    );
+  }
+}
+
+class NoteHome extends StatefulWidget {
+  const NoteHome({Key? key}) : super(key: key);
+
+  @override
+  State<NoteHome> createState() => _NoteHomeState();
+}
+
+class _NoteHomeState extends State<NoteHome> {
+  final TextEditingController noteController = TextEditingController();
+  List<String> notes = []; // Notları saklamak için liste
+
+  void _addNote() {
+    String note = noteController.text;
+    if (note.isNotEmpty) {
+      setState(() {
+        notes.add(note);
+      });
+      noteController.clear();
+    }
+  }
+
+  void _deleteNote() {
+    if (notes.isNotEmpty) {
+      setState(() {
+        notes.removeLast();
+      });
+      noteController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Notes')), 
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: noteController,
+              maxLines: 10,
+              decoration: InputDecoration(
+                hintText: 'Write your note here...',
+                filled: true,
+                fillColor: Colors.black54,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _addNote,
+                  child: const Text('Save'),
+                ),
+                ElevatedButton(
+                  onPressed: _deleteNote,
+                  child: const Text('Delete'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: notes.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.cyan,
+                    child: ListTile(
+                      title: Text(notes[index]),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
